82c7b5f211bf8bfdc4fd0e858138c06925e52404
Warty <ItzWarty@gmail.com>
2016-02-11 11:57:08 -0800

    Add BernieApp.CLI for basic CLI testing

---
+++ BernieApp.CLI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using BernieApp.Common.Http;
+
+namespace BernieApp.CLI {
+   class Program {
+      static void Main(string[] args) {
+         var client = new BernieHttpClient();
+         var issues = client.GetIssuesAsync().Result.ToList();
+         for (int i = 0; i < issues.Count; i++) {
+            var issue = issues[i];
+            Console.WriteLine(i + ": " + issue.Id + " " + issue.Index + " " + issue.Score + " " + issue.Type + issue.Source.Title);
+         }
+         var n = int.Parse(Console.ReadLine());
+         Console.Clear();
+         Console.WriteLine(issues[n].Source.Title);
+         Console.WriteLine(issues[n].Source.Body);
+      }
+   }
+}
