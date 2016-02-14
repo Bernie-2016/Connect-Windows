333ec561a85f805ad3952e3cb37e992298e71560
Riley Taylor <bassclarinet42@gmail.com>
2016-02-14 10:37:53 -0700

    VisualStates Fix for ActionsPage (UWP)
    Added the visual states that keep the header from being hidden by the
    Hamburger icon while in collapsed mode.

--- BernieApp.UWP/View/Shell.xaml.cs
+++ BernieApp.UWP/View/Shell.xaml.cs
@@ -1,23 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Template10.Common;
 using Template10.Controls;
 using Template10.Services.NavigationService;
-using Windows.UI.Core;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace BernieApp.UWP.View
 {
@@ -34,7 +18,7 @@ namespace BernieApp.UWP.View
             Loaded += Shell_Loaded;
         }
 
-        private void Shell_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private void Shell_Loaded(object sender, RoutedEventArgs e)
         {
             
         }
