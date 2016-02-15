be20a0e8e8a3b41cc24296e2fdedbedd67bb70e8
Riley Taylor <bassclarinet42@gmail.com>
2016-02-14 23:29:34 -0700

    Added new PCL, Moved ViewModels to UPW and 8.1 apps, Implemented ViewModelLocator for UWP

---
+++ BernieApp.UWP/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Common;
+using Template10.Services.NavigationService;
+using Windows.UI.Xaml.Navigation;
+
+namespace BernieApp.UWP.ViewModels
+{
+    public class MainViewModel : GalaSoft.MvvmLight.ViewModelBase, Template10.Services.NavigationService.INavigable
+    {
+        public Task OnNavigatedToAsync(object parameter, NavigationMode mode,
+        IDictionary<string, object> state)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnNavigatedFromAsync(IDictionary<string, object> state,
+            bool suspending)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnNavigatingFromAsync(NavigatingEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+
+        [JsonIgnore]
+        public IDispatcherWrapper Dispatcher { get; set; }
+
+        [JsonIgnore]
+        public INavigationService NavigationService { get; set; }
+
+        [JsonIgnore]
+        public IStateItems SessionState { get; set; }
+    }
+}
---
+++ BernieApp.UWP/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BernieApp.UWP.ViewModels
+{
+    public class ViewModelLocator
+    {
+        private NewsViewModel _NewsViewModel;
+        public NewsViewModel NewsViewModel =>
+            _NewsViewModel ?? (_NewsViewModel = new NewsViewModel());
+
+        private ActionsViewModel _ActionsViewModel;
+        public ActionsViewModel ActionsViewModel =>
+            _ActionsViewModel ?? (_ActionsViewModel = new ActionsViewModel());
+
+        private NearbyViewModel _NearbyViewModel;
+        public NearbyViewModel NearbyViewModel =>
+            _NearbyViewModel ?? (_NearbyViewModel = new NearbyViewModel());
+
+        private IssuesViewModel _IssuesViewModel;
+        public IssuesViewModel IssuesViewModel =>
+            _IssuesViewModel ?? (_IssuesViewModel = new IssuesViewModel());
+
+        private SettingsViewModel _SettingsViewModel;
+        public SettingsViewModel SettingsViewModel =>
+            _SettingsViewModel ?? (_SettingsViewModel = new SettingsViewModel());
+    }
+}
