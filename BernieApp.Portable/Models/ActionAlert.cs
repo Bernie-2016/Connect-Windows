acd6095ea270d1d7184332411537f00daecc8a47
Riley Taylor <bassclarinet42@gmail.com>
2016-02-15 13:00:27 -0700

    Add Models

---
+++ BernieApp.Portable/Models/ActionAlert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BernieApp.Portable.Models
+{
+    public class ActionAlert
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+        public string TargetUrl { get; set; }
+        public string TwitterUrl { get; set; }
+        public int TweetId { get; set; }
+    }
+}
---
+++ BernieApp.Portable/Models/Issue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BernieApp.Portable.Models
+{
+    public class Issue
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string ImageUrl { get; set; }
+        public string Url { get; set; }
+
+    }
+}
