f444fe291898a6089d4c41e253aa4c8feb7c0b36
Riley Taylor <rileyhtaylor@outlook.com>
2016-02-15 20:22:02 -0700

    Implemented SimpleIoc in the ViewModelLocator

--- BernieApp.UWP/ViewModels/ViewModelLocator.cs
+++ BernieApp.UWP/ViewModels/ViewModelLocator.cs
@@ -3,14 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using BernieApp.Common.Http;
 
 namespace BernieApp.UWP.ViewModels
 {
     public class ViewModelLocator
     {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                //Create Design Time clients to use here instead of the runtime ones
+                SimpleIoc.Default.Register<NewsClient>();
+                SimpleIoc.Default.Register<IssuesClient>();
+                //Add Actions client
+                //Add Nearby client
+            }
+            else
+            {
+                SimpleIoc.Default.Register<NewsClient>();
+                SimpleIoc.Default.Register<IssuesClient>();
+                //Add Actions client
+                //Add Nearby client
+            }
+            SimpleIoc.Default.Register<NewsViewModel>();
+            SimpleIoc.Default.Register<IssuesViewModel>();
+            SimpleIoc.Default.Register<ActionsViewModel>();
+            SimpleIoc.Default.Register<NearbyViewModel>();
+        }
+
+
         private NewsViewModel _NewsViewModel;
         public NewsViewModel NewsViewModel =>
             _NewsViewModel ?? (_NewsViewModel = new NewsViewModel());
+
+        private IssuesViewModel _IssuesViewModel;
+        public IssuesViewModel IssuesViewModel =>
+            _IssuesViewModel ?? (_IssuesViewModel = new IssuesViewModel());
 
         private ActionsViewModel _ActionsViewModel;
         public ActionsViewModel ActionsViewModel =>
@@ -19,10 +53,6 @@ namespace BernieApp.UWP.ViewModels
         private NearbyViewModel _NearbyViewModel;
         public NearbyViewModel NearbyViewModel =>
             _NearbyViewModel ?? (_NearbyViewModel = new NearbyViewModel());
-
-        private IssuesViewModel _IssuesViewModel;
-        public IssuesViewModel IssuesViewModel =>
-            _IssuesViewModel ?? (_IssuesViewModel = new IssuesViewModel());
 
         private SettingsViewModel _SettingsViewModel;
         public SettingsViewModel SettingsViewModel =>
