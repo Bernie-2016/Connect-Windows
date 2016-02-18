deb0e31dc68d7c9a1a70d0398f8db45ebd3ec56a
Riley Taylor <rileyhtaylor@outlook.com>
2016-02-17 22:19:45 -0700

    Add FeedEntry, commented out NewsArticle and Issue for future removal, Update BernieClient to reflect model change

---
+++ BernieApp.Portable/Models/FeedEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BernieApp.Portable.Models
+{
+    public class FeedEntry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Body { get; set; }
+        public string Excerpt { get; set; }
+        public string ImageUrl { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+    }
+}
--- BernieApp.Portable/Models/NewsArticle.cs
+++ BernieApp.Portable/Models/NewsArticle.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace BernieApp.Portable.Models
-{
-    public class NewsArticle
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public DateTime Date { get; set; }
-        public string Body { get; set; }
-        public string Excerpt { get; set; }
-        public string ImageUrl { get; set; }
-        public string Url { get; set; }
-        public string Description { get; set; }
-    }
-}
+//namespace BernieApp.Portable.Models
+//{
+//    public class NewsArticle
+//    {
+//        public int Id { get; set; }
+//        public string Title { get; set; }
+//        public DateTime Date { get; set; }
+//        public string Body { get; set; }
+//        public string Excerpt { get; set; }
+//        public string ImageUrl { get; set; }
+//        public string Url { get; set; }
+//        public string Description { get; set; }
+//    }
+//}
