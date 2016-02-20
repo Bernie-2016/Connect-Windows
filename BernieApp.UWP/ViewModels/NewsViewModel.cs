88eda853ad39fc094410d3ded244499a62209ac9
Riley Taylor <bassclarinet42@gmail.com>
2016-02-20 11:40:01 -0700

    Added a relay command to refresh the News feed via refresh button

--- BernieApp.UWP/ViewModels/NewsViewModel.cs
+++ BernieApp.UWP/ViewModels/NewsViewModel.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using BernieApp.Portable.Client;
 using BernieApp.Portable.Models;
+using BernieApp.UWP.View;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Template10.Utils;
+using GalaSoft.MvvmLight.Command;
 
 namespace BernieApp.UWP.ViewModels {
     public class NewsViewModel : MainViewModel
     {
         private readonly ObservableCollection<FeedEntry> _items = new ObservableCollection<FeedEntry>();
         private readonly IBernieClient _client;
+        private RelayCommand _loadCommand;
 
         public NewsViewModel(IBernieClient client)
         {
@@ -25,5 +28,25 @@ namespace BernieApp.UWP.ViewModels {
         }
 
         public ObservableCollection<FeedEntry> Items => _items;
+
+        //RelayCommand to allow for refresh of listview
+        public RelayCommand LoadCommand
+        {
+            get
+            {
+                if (_loadCommand == null)
+                {
+                    _loadCommand = new RelayCommand(async () =>
+                    {
+                        //Clears the list, then adds from the server. TODO: A way to add only new items would probably be better.
+                        var news = await _client.GetNewsAsync();
+                        _items.Clear();
+                        _items.AddRange(news);
+                    });
+                }
+                return _loadCommand;
+            }
+        }
+
     }
 }
