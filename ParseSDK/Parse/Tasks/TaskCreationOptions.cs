3251e3826865d95fc15748be4d943975938e246b
Riley Taylor <bassclarinet42@gmail.com>
2016-04-25 20:15:26 -0700

    Add newer Parse SDK that works with UWP

---
+++ ParseSDK/Parse/Tasks/TaskCreationOptions.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Threading.Tasks {
+  internal enum TaskCreationOptions {
+    None
+  }
+}
