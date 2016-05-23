45566137e8db61d89b97265d9a4a4b5f08e7e18a
Riley Taylor <bassclarinet42@gmail.com>
2016-05-22 20:40:08 -0700

    OnStartAync streamlining

--- BernieApp.UWP/App.xaml.cs
+++ BernieApp.UWP/App.xaml.cs
@@ -77,64 +77,21 @@ namespace BernieApp.UWP
             }
             else
             {
-                this.NavigationService.Navigate(typeof(NewsPage));
+                //uncomment to see welcome message again
+                //localSettings.Values.Remove("lastRunDate");
+                var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+                object value = localSettings.Values["lastRunDate"];
+
+                if (value == null)
+                {
+                    NavigationService.Navigate(typeof(WelcomePage));
+                }
+                else
+                {
+                    NavigationService.Navigate(typeof(NewsPage));
+                }
             }
             
-            switch (DetermineStartCause(args))		
-            {		          
- -                case AdditionalKinds.Primary:
- -                    NavigationService.Navigate(typeof(NewsPage));		
- -                    break;		
- -                case AdditionalKinds.Toast:		
- -                    var toastArgs = args as ToastNotificationActivatedEventArgs; //This may be depricated		
- -                    NavigationService.Navigate(typeof(ActionsPage), toastArgs.Argument);		
- -                    break;		
- -                case AdditionalKinds.SecondaryTile:		
- -                    break;		
- -                case AdditionalKinds.Other:		
- -                    NavigationService.Navigate(typeof(NewsPage));		
- -                    break;		
- -                case AdditionalKinds.JumpListItem:		
- -                    break;		
- -                default:		
- -                    NavigationService.Navigate(typeof(NewsPage));		
- -                    break;
-
-            switch (DetermineStartCause(args))
-            {
-                case AdditionalKinds.Primary:
-                    //uncomment to see welcome message again
-                    //localSettings.Values.Remove("lastRunDate");
-                    var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
-                    object value = localSettings.Values["lastRunDate"];
-
-                    if (value == null)
-                    {
-                        NavigationService.Navigate(typeof(WelcomePage));
-                    }
-                    else
-                    {
-                        NavigationService.Navigate(typeof(NewsPage));
-                    }
-
-                    break;
-                case AdditionalKinds.Toast:
-                    var toastArgs = args as ToastNotificationActivatedEventArgs; //This may be depricated
-                    NavigationService.Navigate(typeof(ActionsPage), toastArgs.Argument);
-                    break;
-                case AdditionalKinds.SecondaryTile:
-                    break;
-                case AdditionalKinds.Other:
-                    NavigationService.Navigate(typeof(NewsPage));
-                    break;
-                case AdditionalKinds.JumpListItem:
-                    break;
-                default:
-                    NavigationService.Navigate(typeof(NewsPage));
-                    break;
-            }
-
-
             return Task.FromResult<object>(null);
         }
 
